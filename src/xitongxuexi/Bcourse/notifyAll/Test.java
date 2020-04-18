efbd856a80234b2ee30dbe3828d09828128df173
PC <you@example.com>
2020-04-18 19:02:36 +0800

    提交

--- src/xitongxuexi/Bcourse/notifyAll/Test.java
+++ src/xitongxuexi/Bcourse/notifyAll/Test.java
@@ -3,6 +3,12 @@ package xitongxuexi.Bcourse.notifyAll;
 import jichu.main;
 import sun.security.action.GetBooleanAction;
 
+/**
+ * 类说明：
+ * 	wait 与notifyAll
+ * 	wait :调用前持有锁，调用wait方法后 锁会被立即释放
+ * 	notifyAll:	调用前持有锁，  而释放锁 必须是在包含notifyAll方法执行完成后 才会释放锁
+ */
 public class Test {
 	public synchronized void notify1() {
 		notifyAll();
@@ -15,7 +21,7 @@ public static void main(String[] args) throws InterruptedException {
 	Thread.sleep(1000);
 	Notify.get().notify1();
 }
-	
+
 }
  class  Notify implements Runnable {
 	 private static Notify notify=new Notify();
